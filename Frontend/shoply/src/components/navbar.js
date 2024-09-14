c3c444ffb679daa27e3ba930bcd498c73e012e64
Michał <80591@student.pb.edu.pl>
2024-09-14 18:33:45 +0200

    Update Navbar component to include navigation functionality on logo and cart icon click.

--- Frontend/shoply/src/components/navbar.js
+++ Frontend/shoply/src/components/navbar.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import {ShoppingCartIcon} from '@heroicons/react/outline'
+import { useNavigate } from 'react-router-dom'
 export const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleCartClick =()=>{
+    navigate("/cart")
+  }
+  const handleLogoClick = () =>{
+    navigate("/")
+  }
   return (
     <div className='w-full h-20 bg-black text-white flex justify-end items-center p-10'>
-        <p className='mr-auto text-2xl font-bold '>Shoply</p>
+        <p className='mr-auto text-2xl font-bold ' onClick={handleLogoClick}>Shoply</p>
         <p className='mr-6'>Michał</p>
-        <ShoppingCartIcon className="size-7 text-white-500" onClick={()=>alert("Kliknieto")} />
+        <ShoppingCartIcon className="size-7 text-white-500" onClick={handleCartClick} />
     </div>
     
   )
