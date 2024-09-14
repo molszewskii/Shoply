0430325a7a83d18a9a71856c0a01ec16f8b74466
Michał <80591@student.pb.edu.pl>
2024-09-14 18:11:56 +0200

    Add ProductDbContext class for managing products in the database

---
+++ Backend/ShoplyBack/ShoplyBack/DbContexts/ProductDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoplyBack.Models;
+
+namespace ShoplyBack.DbContexts
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(10,2)");
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
