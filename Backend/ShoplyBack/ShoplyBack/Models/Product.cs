3c1ce0d23195391688ad810a18fc67d15ee14799
Michał <80591@student.pb.edu.pl>
2024-09-14 18:14:52 +0200

    Add Product model class with properties and data annotations

---
+++ Backend/ShoplyBack/ShoplyBack/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShoplyBack.Models
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        public string? ImagePath { get; set; }
+    }
+}
