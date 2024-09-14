14748c2aa1cabe857dd5f3c2031925e357dd9c04
Michał <80591@student.pb.edu.pl>
2024-09-14 18:12:47 +0200

    Add ProductDto class for data transfer with validation

---
+++ Backend/ShoplyBack/ShoplyBack/Dtos/ProductDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoplyBack.Dtos
+{
+    public class ProductDto
+    {
+        public int ? Id { get; set; }
+        public string ? Name { get; set; }
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than zero.")]
+        public decimal Price { get; set; }
+        public string ? ImagePath { get; set; }
+
+    }
+}
