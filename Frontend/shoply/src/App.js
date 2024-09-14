f94aefd1d3f3521c58ed3dea7aa3dd9898acc381
Michał <80591@student.pb.edu.pl>
2024-09-14 18:32:41 +0200

    Update App.js to include new context providers and routing for HomePage and CartPage components.

--- Frontend/shoply/src/App.js
+++ Frontend/shoply/src/App.js
@@ -2,19 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import {Navbar} from "./components/navbar"
+import { ProductProvider } from './contexts/productContext';
+import ProductList from './components/productList';
+import { ShopContextProvider } from './contexts/shopContext';
+import { CartPage } from './pages/CartPage';
+import HomePage from './pages/HomePage';
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path="/" />
-          <Route path="/cart"/>
-          <Route path="/login"/>
-          <Route path="/register"/>
-        </Routes>
-      </Router>
-    </div>
+    <ProductProvider>
+      <ShopContextProvider>
+      <div className="App">
+        <Router>
+          <Navbar/>
+          <Routes>
+            <Route path="/" element={<HomePage/>} />
+            <Route path="/cart" element={<CartPage/>}/>
+            <Route path="/login"/>
+            <Route path="/register"/>
+          </Routes>
+        </Router>
+      </div>
+      </ShopContextProvider>
+    </ProductProvider>
+  
   );
 }
 
