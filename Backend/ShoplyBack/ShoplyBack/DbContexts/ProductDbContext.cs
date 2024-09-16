7ec03e803a6a1500bacc1aadd2d0288fcb2d83fe
Michał <80591@student.pb.edu.pl>
2024-09-16 16:02:35 +0200

    Update ProductDbContext to include Category and DeviceModel DbSets and relationships

--- Backend/ShoplyBack/ShoplyBack/DbContexts/ProductDbContext.cs
+++ Backend/ShoplyBack/ShoplyBack/DbContexts/ProductDbContext.cs
@@ -8,13 +8,23 @@ namespace ShoplyBack.DbContexts
         public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options) { }
 
         public DbSet<Product> Products { get; set; }
-
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<DeviceModel> DeviceModels { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Product>()
                 .Property(p => p.Price)
                 .HasColumnType("decimal(10,2)");
 
+            modelBuilder.Entity<Category>()
+                .HasMany(c => c.Products)
+                .WithOne(p => p.Category)
+                .HasForeignKey(p => p.CategoryId);
+
+            modelBuilder.Entity<DeviceModel>()
+               .HasMany(d => d.Products)
+               .WithOne(p => p.DeviceModel)
+               .HasForeignKey(p => p.DeviceModelId);
 
             base.OnModelCreating(modelBuilder);
         }
