022a0f272ed7a9cb399c4171f45db3c9568a32a4
Michał <80591@student.pb.edu.pl>
2024-09-16 15:58:46 +0200

    Add Category model for category entity

---
+++ Backend/ShoplyBack/ShoplyBack/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace ShoplyBack.Models
+{
+    public class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string CategoryName { get; set; }
+        [JsonIgnore]
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
