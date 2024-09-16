ea94a8684529ef7edf1af7f3aea8de427d45d762
Michał <80591@student.pb.edu.pl>
2024-09-16 16:47:47 +0200

    Refactor CartItemCard component: Replace Context API state management with Redux for cart functionality

--- Frontend/shoply/src/components/cartItemCard.js
+++ Frontend/shoply/src/components/cartItemCard.js
@@ -1,9 +1,19 @@
 import React from 'react'
-import { useShopContext } from '../contexts/shopContext';
+import { useDispatch } from 'react-redux';
+import { addItemToCart, removeItemFromCart } from '../redux/slices/cartSlice';
 
 export const CartItemCard = (props) => {
     const {id, productName, price, productImage, amount} = props;
-    const {addItemToCart, removeItemFromCart} = useShopContext()
+    const dispatch = useDispatch();
+
+    const handleAddItem=()=>{
+        dispatch(addItemToCart(id));
+    }
+
+    const handleRemoveItem=()=>{
+        dispatch(removeItemFromCart(id));
+    }
+
     return (
         <div key={id} className="border rounded-lg p-4 shadow-lg w-4/5 mb-3">
             <img 
@@ -14,9 +24,9 @@ export const CartItemCard = (props) => {
             <h2 className="text-lg font-semibold">{productName}</h2>
             <p className="text-gray-700">${price}</p>
             <div className='flex justify-center items-center'>
-                <p className='p-2 text-xl' onClick={()=>removeItemFromCart(id)}>-</p>
+                <p className='p-2 text-xl' onClick={handleRemoveItem}>-</p>
                 <p className='w-8 h-8 border rounded shadow-lg flex items-center justify-center'>{amount}</p>
-                <p className='p-2 text-xl' onClick={()=>addItemToCart(id)}>+</p>
+                <p className='p-2 text-xl' onClick={handleAddItem}>+</p>
             </div>
             <button className="text-white bg-black p-2 rounded w-1/3 mt-3">Buy Now (${(price*amount).toFixed(2)})</button>
         </div>
