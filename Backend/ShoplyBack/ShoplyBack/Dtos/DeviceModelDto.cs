00e9a4aed69721b1292ce79817ff7c8406b9251b
Michał <80591@student.pb.edu.pl>
2024-09-16 15:57:42 +0200

    Create DeviceModelDto for device model data transfer

---
+++ Backend/ShoplyBack/ShoplyBack/Dtos/DeviceModelDto.cs
@@ -0,0 +1,20 @@
+﻿using ShoplyBack.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoplyBack.Dtos
+{
+    public class DeviceModelDto
+    {
+        public int ? Id { get; set; }
+
+        [Required]
+        public string ModelName { get; set; }
+
+        public string? Specifications { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+    }
+}
