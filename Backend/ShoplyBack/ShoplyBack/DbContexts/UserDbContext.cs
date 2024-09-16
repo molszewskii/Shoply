3347f6d0d8beab9a6b36714ca203d995a99f9a5f
Michał <80591@student.pb.edu.pl>
2024-09-17 00:57:56 +0200

    Add DbSet<User> and extend OnModelCreating in UserDbContext

--- Backend/ShoplyBack/ShoplyBack/DbContexts/UserDbContext.cs
+++ Backend/ShoplyBack/ShoplyBack/DbContexts/UserDbContext.cs
@@ -1,6 +1,17 @@
-﻿namespace ShoplyBack.DbContexts
+﻿using Microsoft.EntityFrameworkCore;
+using ShoplyBack.Models;
+
+namespace ShoplyBack.DbContexts
 {
-    public class UserDbContext
+    public class UserDbContext : DbContext
     {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); 
+        }
     }
 }
