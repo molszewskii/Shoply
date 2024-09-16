8f10073c2f322f578807f744137e7ab869428b49
Michał <80591@student.pb.edu.pl>
2024-09-16 15:58:50 +0200

    Add DeviceModel model for device model entity

---
+++ Backend/ShoplyBack/ShoplyBack/Models/DeviceModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoplyBack.Models
+{
+    public class DeviceModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string ModelName { get; set; }
+
+        public string? Specifications { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; }
+
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
