798fd4af19c4d22493be23c2ebcd167db74db597
Michał <80591@student.pb.edu.pl>
2024-09-16 15:57:18 +0200

    Add CategoryController with endpoints for creating and retrieving categories

---
+++ Backend/ShoplyBack/ShoplyBack/Controllers/CategoryController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShoplyBack.DbContexts;
+using ShoplyBack.Dtos;
+using ShoplyBack.Models;
+
+namespace ShoplyBack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ProductDbContext _productDbContext;
+
+        public CategoryController(ProductDbContext productDbContext)
+        {
+            _productDbContext = productDbContext;
+        }
+
+        [HttpPost]
+        [Route("createCategory")]
+        public async Task<IActionResult> CreateCategory(CategoryDto categoryDto)
+        {
+            if (categoryDto == null)
+            {
+                return BadRequest("Category object is null");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var category = new Category
+            {
+                CategoryName = categoryDto.CategoryName
+            };
+
+            _productDbContext.Categories.Add(category);
+            await _productDbContext.SaveChangesAsync();
+            return Ok(category);
+
+        }
+
+        [HttpGet]
+        [Route("getCategories")]
+        public async Task<IActionResult> GetCategories()
+        {
+            var categories = await _productDbContext.Categories.ToListAsync();
+            if(categories == null || categories.Count == 0)
+            {
+                return NotFound("Categories were not found");
+            }
+            return Ok(categories);
+        }
+    }
+}
