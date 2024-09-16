2828e17efa15f9a60acc887ecbf705dabf61a948
Michał <80591@student.pb.edu.pl>
2024-09-16 15:57:39 +0200

    Create CategoryDto for category data transfer

---
+++ Backend/ShoplyBack/ShoplyBack/Dtos/CategoryDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoplyBack.Dtos
+{
+    public class CategoryDto
+    {
+        public int ? Id { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "Category name cannot be longer than 100 characters.")]
+        public string CategoryName { get; set; }
+
+    }
+}
