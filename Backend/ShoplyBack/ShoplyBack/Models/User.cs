093af1e824281f1ada4383c28d7fb75dcce93a18
Michał <80591@student.pb.edu.pl>
2024-09-17 00:54:28 +0200

    Add properties and data annotations to User class

--- Backend/ShoplyBack/ShoplyBack/Models/User.cs
+++ Backend/ShoplyBack/ShoplyBack/Models/User.cs
@@ -1,6 +1,16 @@
-﻿namespace ShoplyBack.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoplyBack.Models
 {
     public class User
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string UserEmail { get; set; }
+        [Required]
+        public string PasswordHash { get; set; }
     }
 }
