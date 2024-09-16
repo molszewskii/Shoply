add8ff37f3306f8f562f3ecb308a7f717c7a7b3f
Michał <80591@student.pb.edu.pl>
2024-09-16 16:19:45 +0200

    Update CartPage to use Redux for state management
    - Replaced use of useShopContext and useProducts with useSelector from Redux.
    - Updated CartPage to fetch cartItems and products from Redux store.
    - Adjusted logic to display cart items based on state managed by Redux.

--- Frontend/shoply/src/pages/CartPage.js
+++ Frontend/shoply/src/pages/CartPage.js
@@ -1,16 +1,15 @@
+import { useSelector } from "react-redux";
 import { CartItemCard } from "../components/cartItemCard";
-import { useProducts } from "../contexts/productContext";
-import { useShopContext } from "../contexts/shopContext"
 
 export const CartPage = () =>{
-    const {cartItems} = useShopContext();
-    const {products} = useProducts();
+    const cartItems = useSelector(state=>state.cart)
+    const products = useSelector(state=>state.products.products)
     console.log(cartItems)
+    console.log(products)
     return(
         <div className=" flex flex-col items-center mt-3">
-            <p></p>
             {products.map((product)=>{
-                if(cartItems[product.id] !== 0){
+                if(cartItems[product.id] > 0){
                     return(
                     <CartItemCard key={product.id} id={product.id} productName={product.name} price={product.price} productImage={product.imagePath} amount={cartItems[product.id]} />
                     )
