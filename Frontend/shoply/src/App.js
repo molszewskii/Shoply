547ce83c0b5b11badbae8d699e76f4a5e130f1f0
Michał <80591@student.pb.edu.pl>
2024-09-19 01:41:10 +0200

    Update App layout: Added flexbox structure with min-h-screen and flex-grow for responsive layout

--- Frontend/shoply/src/App.js
+++ Frontend/shoply/src/App.js
@@ -5,23 +5,26 @@ import { CartPage } from './pages/CartPage';
 import HomePage from './pages/HomePage';
 import { Provider } from 'react-redux';
 import store from './redux/store'
+import { LoginPage } from './pages/LoginPage';
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
+      <div className="App flex flex-col min-h-screen">
         <Router>
-          <Navbar/>
-          <Routes>
-            <Route path="/" element={<HomePage/>} />
-            <Route path="/cart" element={<CartPage/>}/>
-            <Route path="/login"/>
-            <Route path="/register"/>
-          </Routes>
+          <Navbar />
+          <div className="flex-grow">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" />
+            </Routes>
+          </div>
         </Router>
       </div>
-      </Provider>
-  
+    </Provider>
   );
+  
 }
 
 export default App;
