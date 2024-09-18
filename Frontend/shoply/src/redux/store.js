0a4ade1902be0e8a6d9bc6253cc203ce1392daff
Michał <80591@student.pb.edu.pl>
2024-09-19 01:46:45 +0200

    Integrate userReducer for managing user authentication state in Redux store

--- Frontend/shoply/src/redux/store.js
+++ Frontend/shoply/src/redux/store.js
@@ -1,11 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit'
 import productReducer from './slices/productSlice';
 import cartReducer from './slices/cartSlice'
-
+import userReducer from './slices/userSlice'
 const store = configureStore({
     reducer: {
         products: productReducer,
         cart: cartReducer,
+        user: userReducer,
     }
 });
 
