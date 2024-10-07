03d5866414bd0f19a9b17fc64cbd90596a234e2b
Michał <80591@student.pb.edu.pl>
2024-10-07 21:28:43 +0200

    Enhance Navbar: Add category dropdown menu and fetch categories from API

--- Frontend/shoply/src/components/navbar.js
+++ Frontend/shoply/src/components/navbar.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {ShoppingCartIcon} from '@heroicons/react/outline'
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux';
+import categoryService from '../services/categoryService';
+import deviceModelService from '../services/deviceModelService';
+import { SlideMenu } from './slideMenu';
+
 export const Navbar = () => {
   const navigate = useNavigate();
   const {name, isLoggedIn} = useSelector(state=>state.user)
+  const [categories, setCategories] = useState([])
+  const [hoveredCategory, setHoveredCategory] = useState(null);
+  const [categoryDetails, setCategoryDetails] = useState([]);
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+  useEffect(()=>{
+    const fetchData =async()=>{
+      try{
+        const data = await categoryService.fetchCategories();
+        setCategories(data);
+      }catch(error){
+        console.error("Failed fetching categories");
+        throw error;
+      }
+    }
+    fetchData();
+  },[])
+
+  const handleCategoryHover = async(categoryId)=>{
+    try{
+      const details = await deviceModelService.getDeviceModels(categoryId);
+      console.log(details)
+      setCategoryDetails(details);
+      setIsMenuVisible(true);
+    }catch(error){
+      console.error("Failed fetching cetegory details");
+      throw error;
+    }
+  }
+  const handleMouseLeave = () => {
+    setIsMenuVisible(false);
+};
   const handleCartClick =()=>{
     navigate("/cart")
   }
@@ -16,7 +51,17 @@ export const Navbar = () => {
   }
   return (
     <div className='w-full h-20 bg-black text-white flex justify-end items-center p-10'>
-        <p className='mr-auto text-2xl font-bold ' onClick={handleLogoClick}>Shoply</p>
+        <p className='text-2xl font-bold mr-auto' onClick={handleLogoClick}>Shoply</p>
+        <div className='w-1/2 flex justify-evenly mr-auto'>
+          {categories.map((category)=>(
+            <button key={category.id} className='' onMouseEnter={()=>handleCategoryHover(category.id)}>{category.categoryName}</button>
+          ))}
+        </div>
+        {isMenuVisible && (
+          <div onMouseEnter={() => setIsMenuVisible(true)} onMouseLeave={handleMouseLeave}>
+            <SlideMenu deviceModels={categoryDetails} isVisible={isMenuVisible} />
+          </div>
+        )}
         {isLoggedIn ? (
           <>
           <p className='mr-6'>{name}</p>
